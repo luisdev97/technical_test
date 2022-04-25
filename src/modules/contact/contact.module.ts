41a9965a89fa3ebe12ed7a270ddb3ec4c8d2521a
luisdev97 <amadormontorodavid@gmail.com>
2022-04-25 22:04:25 +0100

    feat: find contacts imported in contacts module

--- src/modules/contact/contact.module.ts
+++ src/modules/contact/contact.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CreateContactService } from './src/application/create-contact/create-contact.service';
+import { FindContactsService } from './src/application/find-contacts/find-contacs.service';
 import { ContactEntity } from './src/domain/entities/contact.entity';
 import { CreateContactController } from './src/infrastructure/controllers/v1/create-contact/create-contact.controller';
 import { FindContactsController } from './src/infrastructure/controllers/v1/find-contacts/find-contacts.controller';
 
 const controllers = [CreateContactController, FindContactsController];
 
-const providers = [CreateContactService];
+const providers = [CreateContactService, FindContactsService];
 
 @Module({
   imports: [TypeOrmModule.forFeature([ContactEntity])],
