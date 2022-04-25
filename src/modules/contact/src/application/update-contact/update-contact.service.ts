736218fe1845c5c11226276607e7110c58e2782e
luisdev97 <amadormontorodavid@gmail.com>
2022-04-25 22:30:27 +0100

    feat: update contact service done

---
+++ src/modules/contact/src/application/update-contact/update-contact.service.ts
@@ -0,0 +1,36 @@
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ContactEntity } from '../../domain/entities/contact.entity';
+import {
+  UpdateContactParamsInputDTO,
+  UpdateContactBodyInputDTO,
+} from '../../infrastructure/controllers/v1/update-contact/update-contact.input.dto';
+import { UpdateContactResponseDTO } from './update-contact.response.dto';
+
+@Injectable()
+export class UpdateContactService {
+  constructor(
+    @InjectRepository(ContactEntity)
+    private readonly contactRepository: Repository<ContactEntity>,
+  ) {}
+
+  async execute(
+    params: UpdateContactParamsInputDTO,
+    body: UpdateContactBodyInputDTO,
+  ): Promise<UpdateContactResponseDTO> {
+    const contact = await this.contactRepository.findOne({
+      where: { id: params.contactId },
+    });
+
+    const updatedContact: ContactEntity = await this.contactRepository.save({
+      ...contact,
+      ...body,
+    });
+
+    return {
+      message: 'Contact updated successfully',
+      status: HttpStatus.OK,
+    };
+  }
+}
