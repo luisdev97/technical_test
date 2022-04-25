1243a7dcc0adef6a6a605d8b62e410bb126732d5
luisdev97 <amadormontorodavid@gmail.com>
2022-04-25 21:31:49 +0100

    test: test to check if create contact service is defined passed

---
+++ src/modules/contact/test/unit/create-contact/create-contact.service.spec.ts
@@ -0,0 +1,41 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { ContactEntity } from '../../../src/domain/entities/contact.entity';
+import { CreateContactService } from '../../../src/application/create-contact/create-contact.service';
+import {
+  CREATE_CONTACT_RESPONSE_FIXTURE,
+  INVALID_CONTACT_INPUT_BODY_FIXTURE,
+  VALID_CONTACT_INPUT_BODY_FIXTURE,
+  VALID_CONTACT_INPUT_PARAMS_FIXTURE,
+} from './create-contact.fixture';
+import { CreateContactParamsError } from '../../../src/domain/errors/create-contact.domain.error';
+
+describe('CreateContactService', () => {
+  let service: CreateContactService;
+
+  beforeEach(async () => {
+    /* eslint-disable @typescript-eslint/no-unused-vars */
+    const mockUserContact = {
+      create: jest.fn().mockImplementation((dto) => dto),
+      save: jest
+        .fn()
+        .mockImplementation((contact) => Promise.resolve({ id: 2 })),
+    };
+
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        CreateContactService,
+        {
+          provide: getRepositoryToken(ContactEntity),
+          useValue: mockUserContact,
+        },
+      ],
+    }).compile();
+
+    service = module.get<CreateContactService>(CreateContactService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
